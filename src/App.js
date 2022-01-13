09dc8b42f1a4ce360723373ddd3e0dd0e0fc1513
John Desrosiers <john.desro@gmail.com>
2022-01-13 05:12:19 -0800

    Initial static layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,146 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Table from "react-bootstrap/Table";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App text-white bg-dark min-vh-100">
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand as="h1">GitHub Manager</Navbar.Brand>
+          <Navbar.Text className="justify-content-end">
+            <Button variant="secondary">Logout</Button>
+          </Navbar.Text>
+        </Container>
+      </Navbar>
+      <Container as="main" className="pt-5">
+        <header>
+          <h2>Repositories</h2>
+        </header>
+        <Form className="pt-5">
+          <Row>
+            <Col lg={6}>
+              <InputGroup className="mb-3">
+                <FormControl
+                  placeholder="Search repo names"
+                  aria-label="Search repo names"
+                  aria-describedby="find-repos-btn"
+                />
+                <Button variant="primary" id="find-repos-btn">
+                  Find repos
+                </Button>
+              </InputGroup>
+            </Col>
+          </Row>
+        </Form>
+        <Row className="mt-3">
+          <Col>
+            <ButtonGroup aria-label="Basic example">
+              <Button variant="danger">Delete</Button>
+              <Button variant="secondary">Clear</Button>
+            </ButtonGroup>
+          </Col>
+        </Row>
+        <Row>
+          <Col lg={8}>
+            <ReposTable />
+          </Col>
+        </Row>
+      </Container>
     </div>
+  );
+}
+
+function ReposTable() {
+  return (
+    <Table variant="dark" size="sm" responsive="sm" className="mt-5">
+      <thead>
+        <tr>
+          <th>Select</th>
+          <th>Name</th>
+          <th>Created</th>
+          <th>Updated</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <div key="checkbox" className="mb-3">
+              <Form.Check type="checkbox" id={`check-api-checkbox`}>
+                <Form.Check.Input
+                  type="checkbox"
+                  aria-label="check to include the repo in the selection for editing"
+                />
+              </Form.Check>
+            </div>
+          </td>
+          <td>
+            <a
+              href="https://github.com/median-man/demo-proj"
+              target="_blank"
+              rel="noreferrer"
+            >
+              demo-proj
+            </a>
+          </td>
+          <td>4-22-2021</td>
+          <td>5-16-2021</td>
+        </tr>
+        <tr>
+          <td>
+            <div key="checkbox" className="mb-3">
+              <Form.Check type="checkbox" id={`check-api-checkbox`}>
+                <Form.Check.Input
+                  type="checkbox"
+                  aria-label="check to include the repo in the selection for editing"
+                />
+              </Form.Check>
+            </div>
+          </td>
+          <td>
+            <a
+              href="https://github.com/median-man/demo-proj"
+              target="_blank"
+              rel="noreferrer"
+            >
+              demo-proj
+            </a>
+          </td>
+          <td>4-22-2021</td>
+          <td>5-16-2021</td>
+        </tr>
+        <tr>
+          <td>
+            <div key="checkbox" className="mb-3">
+              <Form.Check type="checkbox" id={`check-api-checkbox`}>
+                <Form.Check.Input
+                  type="checkbox"
+                  aria-label="check to include the repo in the selection for editing"
+                />
+              </Form.Check>
+            </div>
+          </td>
+          <td>
+            <a
+              href="https://github.com/median-man/demo-proj"
+              target="_blank"
+              rel="noreferrer"
+            >
+              demo-proj
+            </a>
+          </td>
+          <td>4-22-2021</td>
+          <td>5-16-2021</td>
+        </tr>
+      </tbody>
+    </Table>
   );
 }
 
